6d768e1a4192ec300e34d812c9067f985dec5285
pascal.leleannec <pascal.leleannec@agilesoft.nc>
2023-07-06 15:33:23 +1100

    mise à jour formation

--- spring-boot-modules/spring-boot-angular/src/main/java/nc/agilesoft/formation/intellij/exercice4/solution/FileTemplateDemo.java
+++ spring-boot-modules/spring-boot-angular/src/main/java/nc/agilesoft/formation/intellij/exercice4/solution/FileTemplateDemo.java
@@ -1,8 +1,7 @@
 package nc.agilesoft.formation.intellij.exercice4.solution;
 
 /**
- * Classe créée le 05/07/2023
+ * Créé par Pascal le 06/07/2023
  */
-public class Exemple {
-
+public class FileTemplateDemo {
 }
