439b8763ca3c7b89dcf58f0e332e9ac2dbe2a0df
pascal.leleannec <pascal.leleannec@agilesoft.nc>
2023-07-06 17:44:17 +1100

    exercice 5

--- spring-boot-modules/spring-boot-angular/src/test/java/nc/agilesoft/formation/intellij/exercice5/solution/PersonneTest.java
+++ spring-boot-modules/spring-boot-angular/src/test/java/nc/agilesoft/formation/intellij/exercice5/solution/PersonneTest.java
@@ -1,6 +1,8 @@
 package nc.agilesoft.formation.intellij.exercice5.solution;
 
 import nc.agilesoft.formation.intellij.exercice5.Personne;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -9,23 +11,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class PersonneTest {
 
+    private Personne personne;
+
+    @BeforeEach
+    void setUp() {
+        personne = new Personne("NOM", "Jean", LocalDate.of(2000, 1, 1));
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
     @Test
     public void calculateAgeADate() {
-        Personne personne = new Personne("NOM", "Jean", LocalDate.of(2000, 1, 1));
         int age = personne.calculateAgeADate(LocalDate.of(2001, 1, 1));
         assertThat(age).isEqualTo(1);
     }
 
     @Test
     public void calculateAgeADate21() {
-        Personne personne = new Personne("NOM", "Jean", LocalDate.of(2000, 1, 1));
         int age = personne.calculateAgeADate(LocalDate.of(2021, 12, 31));
         assertThat(age).isEqualTo(21);
     }
 
     @Test
     public void calculateAgeKo() {
-        Personne personne = new Personne("NOM", "Jean", LocalDate.of(2000, 1, 1));
         int age = personne.calculateAgeADate(LocalDate.of(2021, 12, 31));
         assertThat(age).isEqualTo(100);
     }
